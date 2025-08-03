c9e259b368ce0ab889f20fe8b7aea09a64778ed5
jess-wang99 <60024187+jess-wang99@users.noreply.github.com>
2025-08-03 10:16:57 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,7 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: '#0077ff',
-        secondary: '#00ffea',
-        accent: '#ff00ff',
-        dark: '#1a202c',
-      },
-      fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
